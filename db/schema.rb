33223dea907a0e680524288cd899a8fcc3e561ce
Martinpouh <martinchege2015@gmail.com>
2022-09-08 17:12:50 +0300

    Generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_120855) do
+ActiveRecord::Schema.define(version: 2022_09_08_135915) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "patient_id"
