dd73cd4a00b534c38a427d0ba064303da53d7394
Martinpouh <martinchege2015@gmail.com>
2022-09-12 08:00:13 +0300

    Add comment in the booking migration file

--- db/migrate/20220908135829_create_bookings.rb
+++ db/migrate/20220908135829_create_bookings.rb
@@ -1,6 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[6.1]
   def change
+    # Create the table
     create_table :bookings do |t|
+      #Attributes
       t.integer :patient_id
       t.integer :consultation_id
   end
