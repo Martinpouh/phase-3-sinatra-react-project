2d2ac5de2d878aa90fd0a8d202b1ab80d1163a6f
Martinpouh <martinchege2015@gmail.com>
2022-09-12 08:02:47 +0300

    Add comment in the  migration files

--- db/migrate/20220908135854_create_add_physician_to_bookings.rb
+++ db/migrate/20220908135854_create_add_physician_to_bookings.rb
@@ -1,5 +1,6 @@
 class CreateAddPhysicianToBookings < ActiveRecord::Migration[6.1]
   def change
+    # add column
     add_column :consultations, :physician, :string
   end
 end
--- db/migrate/20220908135915_create_change_date_time_to_string.rb
+++ db/migrate/20220908135915_create_change_date_time_to_string.rb
@@ -1,5 +1,6 @@
 class CreateChangeDateTimeToString < ActiveRecord::Migration[6.1]
   def change
+    # change column
     change_column :consultations, :time, :string
     add_column :consultations, :date, :string
   end
