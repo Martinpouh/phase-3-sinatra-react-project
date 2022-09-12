471ed3bf2eb55a4308f3a6359729487c266aa825
Martinpouh <martinchege2015@gmail.com>
2022-09-12 08:01:11 +0300

    Add comment in the consultation migration file

--- db/migrate/20220908135834_create_consultations.rb
+++ db/migrate/20220908135834_create_consultations.rb
@@ -1,6 +1,8 @@
 class CreateConsultations < ActiveRecord::Migration[6.1]
   def change
+    # Create the table
     create_table :consultations do |t|
+      #Attributes
       t.string :name
       t.string :description
       t.datetime :time
