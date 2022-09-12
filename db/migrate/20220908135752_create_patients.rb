f643f895d59c69959aabe810150fe948b386db69
Martinpouh <martinchege2015@gmail.com>
2022-09-12 07:59:18 +0300

    Add comment in the patient migration file

--- db/migrate/20220908135752_create_patients.rb
+++ db/migrate/20220908135752_create_patients.rb
@@ -1,6 +1,8 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
+    # Create the table
     create_table :patients do |t|
+      #Attributes
       t.string :first_name
       t.string :last_name
       t.string :email
